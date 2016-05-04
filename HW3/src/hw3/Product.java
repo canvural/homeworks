c112b8139ebba1616116efd070356d5a945986b3
canvural <can9119@gmail.com>
2016-05-04 20:56:43 +0200

    Finish HW3

--- HW3/src/hw3/Product.java
+++ HW3/src/hw3/Product.java
@@ -1,6 +1,113 @@
 package hw3;
 
+import java.security.InvalidParameterException;
+
 public class Product
 {
+    private int productId;
+    private String name;
+    private double price;
+    private double quantity;
+    public boolean isPartial;
+    
+    public Product(int id, String name, double price, double quantity, boolean partial)
+    {
+        this.productId = id;
+        this.name = name;
+        this.price = price;
+        this.isPartial = partial;
+        
+        this.setQuantity(quantity);
+    }
 
+    public int getId()
+    {
+        return productId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        if(price < 0.0) {
+            System.out.println("Price can't be negative");
+        }
+        
+        this.price = price;
+    }
+
+    public double getQuantity()
+    {
+        return quantity;
+    }
+
+    public final void setQuantity(double quantity)
+    {
+        if(this.isPartial) {
+            quantity = Math.floor(quantity);
+        }
+        
+        this.quantity = quantity;
+    }
+    
+    public boolean reduceQuantity(double amount)
+    {
+        if(amount < 0.0) {
+            System.out.println("Quantity can't be negative");
+            return false;
+        }
+        
+        if(amount > this.quantity) {
+            System.out.println("Amount should be less then or equal to remaning quantity.");
+            return false;
+        }
+        
+        if(this.isPartial && amount % 1 != 0) {
+            System.out.println("Whole number required!");
+            return false;
+        }
+        
+        this.quantity -= amount;
+        
+        return true;
+    }
+    
+    public boolean increaseQuantity(double amount)
+    {
+        if(amount < 0.0) {
+            System.out.println("Quantity can't be negative");
+            return false;
+        }
+        
+        if(this.isPartial && amount % 1 != 0) {
+            System.out.println("Whole number required!");
+            return false;
+        }
+        
+        this.quantity += amount;
+        
+        return true;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return this.getId() + "\t"
+                + this.getName() + "\t"
+                + this.getQuantity() + "\t"
+                + this.getPrice();
+    }
 }
--- HW3/src/hw3/Store.java
+++ HW3/src/hw3/Store.java
@@ -2,5 +2,86 @@ package hw3;
 
 public class Store
 {
+    public String name;
+    public Product[] products;
+    
+    public Store(String name, Product[] products)
+    {
+        this.name = name;
+        this.products = products;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public void productList()
+    {
+        System.out.println("ID\tName\tQty\tPrice");
+        
+        for(Product p : this.products) {
+            System.out.println(p);
+        }
+    }
+    
+    public double sellProduct(int id, double amount)
+    {
+        Product p = getProductById(id);
+        if(null == p) {
+            System.out.println("Product not found!");
+            return -1;
+        }
+        
+        if(!p.reduceQuantity(amount)) {
+            return -1;
+        }
+        
+        System.out.println(p.getName() + ": " + amount +
+                " sold at " + p.getPrice()
+        );
+        
+        return p.getPrice() * amount;
+    }
+    
+    public boolean addQuantity(int id, double amount)
+    {
+        Product p = getProductById(id);
+        if(null == p) {
+            System.out.println("Product not found!");
+            return false;
+        }
+        
+        if(!p.increaseQuantity(amount)) {
+            return false;
+        }
+        
+        System.out.println(p.getName() + ": " + amount +
+                " added"
+        );
+        
+        return true;
+    }
+    
+    public void discount(int id, double percentage)
+    {
+        Product p = getProductById(id);
+        if(null == p) {
+            System.out.println("Product not found!");
+            return;
+        }
+        
+        p.setPrice(p.getPrice() * (1 - percentage));
+    }
 
+    private Product getProductById(int id)
+    {
+        for(Product p : this.products) {
+            if(p.getId() == id) {
+                return p;
+            }
+        }
+        
+        return null;
+    }
 }
